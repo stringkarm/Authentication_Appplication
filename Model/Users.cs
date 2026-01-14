811dd70fae7935eff1ba9579604131b4386d9c7c
karms <ACT-STUDENT@LAB2-PC32>
2026-01-14 17:52:47 +0800

    added new Razor Pages Layout and razor pages empty to authenticated the username and password

---
+++ Model/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Authentication_Appplication.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,25 @@
+
+using Authentication_Appplication.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Authentication_Appplication.Pages
+
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]      
+        public Users Users { get; set; } = new Users();
+
+        public IActionResult OnPost()
+        {
+            if(Users.Username == "admin" && Users.Password == "admin123" )
+            {
+                return RedirectToPage("/Index");
+            }
+            return Page();
+         
+
+        }
+    }
+}
